b0b6b218a195032c81abe0b6dd893dc840ab16a7
JoseLinux <josearoldo.de@utp.edu.co>
2026-05-02 01:12:43 -0500

    add - logica reemplazar letras correctas

--- Python--TOTAL--Programador-Avanzado-en-16-días/Dia 5/ProyectoAhorcado/Main.py
+++ Python--TOTAL--Programador-Avanzado-en-16-días/Dia 5/ProyectoAhorcado/Main.py
@@ -7,30 +7,36 @@ def Main():
 
     palabra= random.choice(listName)
     numberLetters= len(list(palabra))#Number of letters that have a word and with that numbre put the "-" in the client
-    print("-" * numberLetters)  # The word in indden mode
-
+    hidden_word = "-" * numberLetters  # The word in hidden mode
+    print(hidden_word)  # The word in hidden mode
     vida = 5  # The life that the user have
 
 
-    letra=input("Ingrese la letra: ")#This is the letter in orden to search
-
     # AGREGAR LETRAS DIGITADAS ANTERIORMENTE
     add_to_list = lambda Lista, letra: Lista.append(letra)  # Funtion para agregar letras al final
-
     #VALIDACIONES PARA SABER SI ES UNA LETRA VALIDAXD
-    validar = lambda x: x.isalpha() and len(list(x)) == 1 and x not in LetrasIngresadas  # Validation to know if it is alphabet and is one letter
+    validar_letra = lambda x: x.isalpha() and len(list(x)) == 1 and x not in LetrasIngresadas  # Validation to know if it is alphabet and is one letter
     # SABER DONDE ESTA LA LETRA
-    posicion = lambda let, pal: pal.find(letra) if validar(let) and let in pal else -1
-    print(f"lista de letras: {LetrasIngresadas}, validar: {validar(letra)}, posicion {posicion}")
-    print(posicion(letra, palabra))
+    posiciones = lambda let, pal: [i for i, l in enumerate(pal) if l == let] if validar_letra(let) else []
 
 
+    while vida > 0:
+        if hidden_word == palabra:
+            print(f"ganaste, la palabra era {palabra}")
+            break
+        letra=input("Ingrese la letra: ")#This is the letter in orden to search
+        if validar_letra(letra) and letra in palabra:
+            print("La letra esta en la palabra")
+            for posicion in posiciones(letra, palabra):
+                hidden_word = hidden_word[:posicion] + letra + hidden_word[posicion + 1:]
+            print(hidden_word)
+        else:
+            print("La letra no esta en la palabra")
+            vida -= 1
+            print(f"Te quedan {vida} vidas")
+        add_to_list(LetrasIngresadas, letra)
 
 
-
-
-    add_to_list(LetrasIngresadas,letra)
 
 
 Main()
-#vida = lambda x: x > 0 para saber si tiene vidas todavia o no
