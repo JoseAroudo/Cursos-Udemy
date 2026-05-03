5a0bb3ef6066b8b3b2c85c6e2e0772ae705b52de
JoseLinux <josearoldo.de@utp.edu.co>
2026-05-02 19:41:17 -0500

    feat - se termina la funcionalidad, opcion 1

--- Python--TOTAL--Programador-Avanzado-en-16-días/Dia 6/ProyectoDelDia6_AdministradorDeRecetas.py
+++ Python--TOTAL--Programador-Avanzado-en-16-días/Dia 6/ProyectoDelDia6_AdministradorDeRecetas.py
@@ -4,26 +4,18 @@ import os
 #base = Path(r"C:\Users\jajap\OneDrive\Desktop\Programacion\CursoUdemy\pythonProject\Dia 6\Recetas")
 
 
-def leer_archivo(archivo):
-    archi=open(archivo)       #Abre el archivo dado
-    print(archi.read())      #Muestra el archivo en pantalla
+leer_archivo= lambda archivo: print(open(archivo).read())
 
-def Ver_categorias():
-    base= os.getcwd()                   #Ruta base hasta "Dia 6"
-    recetas= Path(base, "Recetas")#A esa ruta base le agrego recetas para que esté dentro de las categorias
-    aver= os.listdir(recetas)           #Mostras las diferentes categorias de comidas
-    print(aver)
-    return recetas
 
-def Elegir_carpeta(carpeta):             #Se hace la función que reciba la categoria de a la que va a entrar
-    base= os.getcwd()                    #Se ubica en el path donde estamos
-    categoria= Path(base, carpeta) #Se agrega la categoria a la que se quiere entrar a el path
-    return categoria
+Ver_categorias = lambda: os.listdir(Path(__file__).resolve().parent / "Recetas")
+
+Elegir_carpeta = lambda carpeta: Path(Path(__file__).resolve().parent, carpeta)
 
 def Ver_recetas(ruta):
     # Buscar archivos .txt recursivamente en todos los subdirectorios dentro de la carpeta
     archivos_txt = list(ruta.rglob("*.txt"))
     num = 0
+    archivos_txt = [archivo.name for archivo in ruta.rglob("*.txt")]
     for archivo in archivos_txt: # Imprimir los archivos encontrados
         num += 1  # Cuenta las recetas
         print(archivo)  # Muestra las recetas
@@ -31,25 +23,54 @@ def Ver_recetas(ruta):
 
 
 def MenuPrincipal():
+    base= Path(__file__).resolve().parent
     print('''############ MENU PRINCIPAL ############ 
     
 [1] leer receta
 [2] crear receta
 [3] crear categoria
 [4] eliminar receta
-[5] eliminar categoria
+[5] eliminar receta
 [6] finalizar programa''')
+    match input("Elige una opción: "):
+        case "1":
+            opcion1(base)
+        case "2":
+            print("Opción 2 seleccionada")
+        case "3":
+            print("Opción 3 seleccionada")
+        case "4":
+            print("Opción 4 seleccionada")
+        case "5":
+            print("Opción 5 seleccionada")
+        case "6":
+            print("Programa finalizado")
+            return 0
+        case _:
+            print("Opción no válida, por favor elige una opción del 1 al 6")
 
 #TODO    Mostrar Recetas
 #TODO    Elegir receta
 #TODO    Leer receta
-def opcio1():
+    
+
+
+def opcion1(base):
     print("\nPor favor, elige una categoría entre las siguientes opciones:\n")
-    Ver_categorias()
-    categoria=input()
-    Ver_recetas(Path(base, categoria))
-    receta=input("Elije una receta: ")
-    leer_archivo(Path(base, categoria,receta+".txt"))
+    print(Ver_categorias())
+    categoria=input().strip()
+
+    ruta= Path(base,"Recetas", categoria)
+
+    archivos_txt = [archivo.name for archivo in ruta.rglob("*.txt")]
+    print(f"\nLas recetas disponibles en esta categoría son:\n {archivos_txt}\n\n")
+    
+    Ver_recetas(Path(base,"Recetas", categoria))
+    receta=input("\n\nElije una receta: ")
+    ruta_receta= Path(base,"Recetas", categoria, receta+".txt")
+    leer_archivo(ruta_receta)
+
 
 MenuPrincipal()
-opcio1()
+#base= os.getcwd() 
+#opcion1(Path(__file__).resolve().parent)
